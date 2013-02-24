53016d048d720e71a0ee01afd3b673773ae64478
Yi Yan Zhou <balzaczyy@gmail.com>
2013-02-24 23:22:56 +0800

    finish zip-seq zip-filter and zip-copy functions

--- src/filter_sync/core.clj
+++ src/filter_sync/core.clj
@@ -1,7 +1,47 @@
 (ns filter-sync.core
   (:gen-class))
 
+(import 
+	java.io.FileInputStream
+	'(java.util.zip ZipEntry ZipInputStream))
+
+(defn zip-seq
+	"Convert zip file into sequence."
+	[filename]
+	(lazy-seq
+		(loop [zin (-> filename FileInputStream. ZipInputStream.) coll '()]
+			(let [entry (.getNextEntry zin)]
+				(if (nil? entry)
+					coll
+					(recur zin (conj coll (.getName entry))))))))
+
+(defn zip-filter
+	"Scan the zip file and keep only matched entries."
+	[filename s]
+	(lazy-seq
+		(filter #(.endsWith % s) (zip-seq filename))))
+
+(import
+	org.apache.commons.io.IOUtils
+	java.io.FileOutputStream
+	'(java.util.zip ZipFile ZipOutputStream))
+
+(defn zip-copy
+	"Copy the zip with given entries."
+	[from to entries]
+	(with-open [zin (ZipFile. from)
+		zout (-> to FileOutputStream. ZipOutputStream.)]
+		(dorun
+			(for [entry entries
+				:let [zipEntry (.getEntry zin entry)]
+				:when zipEntry]
+				(do
+					(println (str "Copying " entry "..."))
+					(.putNextEntry zout zipEntry)
+					(IOUtils/copy (.getInputStream zin zipEntry) zout))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (zip-copy "sample/simple/doc.zip" "sample/simple/out/doc.zip" '("a.txt" "b.gif")))
--- test/filter_sync/core_test.clj
+++ test/filter_sync/core_test.clj
@@ -2,6 +2,24 @@
   (:use clojure.test
         filter-sync.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest testZipSeq
+	(testing "There are two files in doc.zip."
+		(= 2 (count (zip-seq "sample/simple/doc.zip")))))
+
+(deftest testZipSeq2
+	(testing "The doc.zip contains: a.txt and b.gif."
+		(= ["a.txt" "b.gif"] (zip-seq "sample/simple/doc.zip"))))
+
+(deftest testZipFilter
+	(testing "There is one text file in doc.zip."
+		(= 1 (count (zip-filter "sample/simple/doc.zip" ".txt")))))
+
+(deftest testZipFilter2
+	(testing "The doc.zip contains one text file: a.txt."
+		(= ["a.txt"] (zip-filter "sample/simple/doc.zip" ".txt"))))
+
+(deftest testZipCopy
+	(testing "Check out/doc.zip if it's identical."
+		(do
+			(zip-copy "sample/simple/doc.zip" "sample/simple/out/doc.zip" '("a.txt" "b.gif"))
+			(= 2 (count (zip-seq "sample/simple/out/doc.zip"))))))

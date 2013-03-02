240090217b3d010df75fe308d0e939a13f3c9325
Yi Yan Zhou <balzaczyy@gmail.com>
2013-03-02 23:20:06 +0800

    add filter copy function

--- src/filter_sync/core.clj
+++ src/filter_sync/core.clj
@@ -40,8 +40,41 @@
 					(.putNextEntry zout zipEntry)
 					(IOUtils/copy (.getInputStream zin zipEntry) zout))))))
 
+(defn zip-filter-copy
+	"Copy the filter with given filter."
+	[from to ext]
+	(zip-copy from to (zip-filter from ext)))
+
+(defn rel-path
+	"Calculate relative path."
+	[source target]
+	(let [len (-> source .getPath .length)
+		s (.getPath target)]
+		(.substring s len)))
+
+(import java.io.File)
+(defn folder-copy
+	"Copy the folder with given entries."
+	[from to entries]
+	(do
+		(.mkdirs to)
+		(dorun
+			(for [file entries
+				:let [rel (rel-path from file)
+				target (File. to rel)]]
+				(if (.isDirectory file)
+					(do
+						(println (str "Creating " target "..."))
+						(.mkdirs target))
+					(do
+						(println (str "Copying " target "..."))
+						(clojure.java.io/copy file target)))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!")
-  (zip-copy "sample/simple/doc.zip" "sample/simple/out/doc.zip" '("a.txt" "b.gif")))
+  (folder-copy (File. "sample/simple") (File. "target/simple")
+  	[(File. "sample/simple/a.txt")
+  	(File. "sample/simple/folder")
+  	(File. "sample/simple/folder/b.gif")]))
--- test/filter_sync/core_test.clj
+++ test/filter_sync/core_test.clj
@@ -23,3 +23,16 @@
 		(do
 			(zip-copy "sample/simple/doc.zip" "sample/simple/out/doc.zip" '("a.txt" "b.gif"))
 			(= 2 (count (zip-seq "sample/simple/out/doc.zip"))))))
+
+(deftest testZipFilterCopy
+	(testing "Check out/doc.zip if it contains only text files."
+		(do
+			(zip-filter-copy "sample/simple/doc.zip" "sample/simple/out/doc.zip" ".txt")
+			(= 1 (count (zip-seq "sample/simple/out/doc.zip"))))))
+
+(deftest testRelativePath
+	(testing "base/path, base/path/file, -> file"
+		(= "file" 
+			(rel-path
+				(clojure.java.io/file "base/path")
+				(clojure.java.io/file "base/path/file")))))
